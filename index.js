010d21b0c6f045f57bdb51a543e525a466090c2b
Alex Duval <alexduval71@gmail.com>
2021-01-20 21:50:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const clear = require('clear-any-console');
+
+clear();
+console.log(`==================
+Alex DUVAL
+
+French FullStack Dev, Vue fanboy, Nuxt addict, JS guy, podcaster, teacher. 
+Live in french alps. 
+https://www.alexduval.fr
+
+🐦 TWITTER  /xlanex6         https://twitter.com/xlanex6
+💻 GITHUB   /xlanex6         https://github.com/xlanex6
+🎙️  PODCAST  /doubleSlash     https://slash-podcast.fr
+
+==================`);
